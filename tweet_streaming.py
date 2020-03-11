73b65112b2bebddb09e16c7b4b7ede958a9e8df4
Kalfa <theo92320@hotmail.fr>
2020-03-11 17:13:16 +0100

    first commit

--- tweet_streaming.py
+++ tweet_streaming.py
@@ -1,18 +1,17 @@
+import os
 import socket
 import sys
 import requests
 import json
 import tweepy
 
-
 # override tweepy.StreamListener to add logic to on_status
 from tweepy import StreamListener, Stream, OAuthHandler
 
-
-ACCESS_TOKEN = '620314216-oqeES5scPU2z5w6lJjdvHiCUJGZsD3RKbw9aW7Hp'
-ACCESS_SECRET = 'NrI437fzy2PBCkBaY9EZA2K5itpQjBj66WqOLQ5mv2k6Q'
-CONSUMER_KEY = '1iWeHX1Zxvh6FBGTj7jV5fYqB'
-CONSUMER_SECRET = 'KGdTYXVj2K5XpPne07gZyqQ63oqaXUtAmEUQKBqEIsFg3kithO'
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
 
 
 class StdOutListener(StreamListener):
@@ -20,14 +19,13 @@ class StdOutListener(StreamListener):
     This is a basic listener that just prints received tweets to stdout.
 
     """
+
     def on_data(self, data):
         print(data)
         return True
 
     def on_error(self, status):
         print(status)
-
-
 
 
 def auth():
@@ -37,15 +35,6 @@ def auth():
 
     stream = Stream(auth, l)
     return stream
-
-
-##def get_tweets():
-##    url = 'https://stream.twitter.com/1.1/statuses/filter.json'
-##    query_data = [('language', 'en'), ('locations', '-130,-20,100,50'), ('track', '#')]
-##    query_url = url + '?' + '&'.join([str(t[0]) + '=' + str(t[1]) for t in query_data])
-##    response = requests.get(query_url, auth=my_auth, stream=True)
-##    print(query_url, response)
-##   return response
 
 
 stream = auth()
